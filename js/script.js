f4f3809673422232254565a066ca3e0ebd82d04d
Ibrahim Abdulqoyyum <ibrahimabdulqoyyum@gmail.com>
2022-05-08 02:02:28 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.querySelectorAll('.course').forEach((course) => {
   course.addEventListener('mouseover', function () {
-    course.classList.remove('hidden');
+    course.children[2].classList.remove('hidden');
+  });
+});
+
+document.querySelectorAll('.course').forEach((course) => {
+  course.addEventListener('mouseout', function () {
+    course.children[2].classList.add('hidden');
   });
 });
